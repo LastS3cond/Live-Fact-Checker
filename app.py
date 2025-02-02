a1d20e7ffe1998432b7c09f180c1c7216a853151
saideepkatragadda <88284767+saideepkatragadda@users.noreply.github.com>
2025-02-01 23:42:47 -0500

    extwact twanscwipt

--- app.py
+++ app.py
@@ -173,3 +173,12 @@ if st.button("Modify Text") and user_text.strip():
 
 elif user_text.strip():
     st.warning("Click the 'Modify Text' button to process your input")
+
+
+def extract_transcript(youtube_video_url):
+    video_id = youtube_video_url.split("=")[1]
+    transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ""
+    for i in transcript_text:
+        transcript += " " + i["text"]
+    return transcript
